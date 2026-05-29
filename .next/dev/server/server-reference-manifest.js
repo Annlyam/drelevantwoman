6620cb50188e881893b7250ab41c533ea0cc6d8e
zoe laenskii <abigailoluchi532@gmail.com>
2026-05-29 10:27:13 +0100

    chore: fix gitignore to exclude .next folder

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VkwhvhLzdpOY4avaXmH3JF6FCHUWa+zunntGUPQ5F+c=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__ccedf4c6._.css",
-  "static/chunks/_96d5293e._.js"
-],
-    source: "dynamic"
-});
