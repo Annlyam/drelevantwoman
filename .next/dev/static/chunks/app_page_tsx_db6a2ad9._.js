36969bfd71d975d9a19803ce043c7264dc7f76d1
zoe laenskii <abigailoluchi532@gmail.com>
2026-05-29 10:15:16 +0100

    feat: build GET /api/events endpoint with past/upcoming filtering

---
+++ .next/dev/static/chunks/app_page_tsx_db6a2ad9._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_19117e40._.js",
+  "static/chunks/node_modules_next_3e323d0b._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_38e2470d._.js",
+  "static/chunks/node_modules_swiper_69bd04c6._.js",
+  "static/chunks/node_modules_9fd3871a._.js",
+  "static/chunks/node_modules_swiper_7704b400._.css"
+],
+    source: "dynamic"
+});
