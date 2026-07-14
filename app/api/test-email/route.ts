eb767409b60de5cbad223b5172e4d81a5d89ab56
techbium <olatunjirauf091@gmail.com>
2026-07-14 10:34:22 +0100

    :) quickPush

---
+++ app/api/test-email/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { brevo } from "@/lib/brevo/client";
+
+export async function GET() {
+    try {
+        const response = await brevo.transactionalEmails.sendTransacEmail({
+            sender: {
+                name: "The Relevant woman",
+                email: process.env.EMAIL_FROM!,
+            },
+            to: [
+                {
+                    email: "YOUR_EMAIL@gmail.com",
+                },
+            ],
+            subject: "Brevo Test",
+            textContent: "if you're reading this, Brevo is working!"
+        });
+        NextResponse.json({
+            success: true,
+            response,
+        });
+
+    }catch(error){
+        console.error(error);
+
+        return NextResponse.json({success:false, error, status: 500})
+    }
+}
