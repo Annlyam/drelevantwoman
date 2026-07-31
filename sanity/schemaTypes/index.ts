270825dbd4c97b662ee7d154c38a816e60e9a12e
kingsleychinonsoeze <kchinonso162@gmail.com>
2026-08-01 04:59:01 +1400

    Add Campaign Email schema and Brevo webhook integration

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -5,7 +5,8 @@ import { blogSchema } from './blog'
 import { academyItemSchema } from './academyItem'
 import { storeProductSchema } from './storeProduct'
 import { teamMemberSchema } from './teamMember'
+import campaignEmailSchema from './campaignEmail'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [eventSchema, blogSchema, academyItemSchema, storeProductSchema, teamMemberSchema],
+  types: [eventSchema, blogSchema, academyItemSchema, storeProductSchema, teamMemberSchema, campaignEmailSchema],
 }
